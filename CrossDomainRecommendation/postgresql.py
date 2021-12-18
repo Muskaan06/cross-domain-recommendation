81a327096586103eeffc21b9de872b10b4ae1eba
Amisha Jodhani <sbgs.amisha.jodhani@gmail.com>
2021-12-18 11:55:33 +0530

    fixed postgresql file

--- CrossDomainRecommendation/postgresql.py
+++ CrossDomainRecommendation/postgresql.py
@@ -10,10 +10,18 @@ conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_
 with conn:
     crsr = conn.cursor()
 
-    sql_command = """CREATE TABLE song_table (
-    id              id SERIAL PRIMARY KEY,
-    song_name       VARCHAR(50),
-    artist_name     VARCHAR(20),
-    UNIQUE (song_name,artist_name));"""
-    crsr.execute(sql_command)
+    # sql_command = """CREATE TABLE song_table (
+    # id              SERIAL PRIMARY KEY,
+    # song_name       VARCHAR(50),
+    # artist_name     VARCHAR(20),
+    # UNIQUE (song_name,artist_name));"""
+    # crsr.execute(sql_command)
+
+    sql_command = """INSERT INTO song_table(song_name,artist_name) VALUES (%s,%s);"""
+    crsr.execute(sql_command, ('perfect', 'ed sheeran'))
     conn.commit()
+
+    sql_disp = '''SELECT * FROM song_table'''
+    crsr.execute(sql_disp)
+
+    print(crsr.fetchall())
