41f3c607466833fd8e8be05eb434d49f653bbc15
Muskaan06 <muskaan.maurya06@gmail.com>
2022-01-06 20:02:31 +0530

    updated random forest

--- CrossDomainRecommendation/spotify_connect.py
+++ CrossDomainRecommendation/spotify_connect.py
@@ -1,9 +1,6 @@
 # import spotipy
 # from spotipy.oauth2 import SpotifyClientCredentials,SpotifyOAuth
 #
-# cid = '5f9054e7c5484b1996ab8f54b97e3156'
-# secret = '81e74abdded4428da073591e14e57884'
-#
 # client_credentials_manager = SpotifyClientCredentials(client_id=cid, client_secret=secret)
 # sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 # sp1 = spotipy.Spotify(auth_manager=SpotifyOAuth(scope="user-library-read",client_id=cid,client_secret=secret,redirect_uri='https://edujtm.github.io/diversify/redirect'))
